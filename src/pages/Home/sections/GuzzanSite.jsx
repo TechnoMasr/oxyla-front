675456b77592145e4f557170fe55e91898c2be8f
Walid Mostafa <amadalioo980@gmail.com>
2026-06-08 10:21:44 +0300

    edit Muzzan banner bg

--- src/pages/Home/sections/GuzzanSite.jsx
+++ src/pages/Home/sections/GuzzanSite.jsx
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import { getHomeSection1 } from "../../../services/homeServices";
 import GuzzanSiteSkeleton from "../../../components/Loading/SkeletonLoading/GuzzanSiteSkeleton";
 import { useTranslation } from "react-i18next";
+import bg from "../../../assets/images/muzzan-bg.jpg";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const GuzzanSite = () => {
   const { t } = useTranslation();
@@ -16,9 +18,14 @@ const GuzzanSite = () => {
   if (!data) return null;
 
   return (
-    <section className="relative my-16 md:my-28 mx-4 max-w-7xl lg:mx-auto rounded-3xl bg-gray-200 border border-gray-100 text-gray-900 overflow-hidden shadow-xl shadow-gray-200/50">
+    <section
+      className="relative my-16 md:my-28 mx-4 max-w-7xl lg:mx-auto rounded-3xl bg-gray-200 
+      border border-gray-100 text-gray-900 overflow-hidden shadow-xl shadow-gray-200/50 bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
       {/* شبكة النقاط الخلفية الهادئة باللون الرمادي الفاتح */}
       <div className="absolute inset-0 rounded-3xl opacity-[0.03] bg-[linear-gradient(to_right,#000_1px,transparent_1px),linear-gradient(to_bottom,#000_1px,transparent_1px)] bg-[size:24px_24px] z-0"></div>
+      <div className="absolute inset-0  bg-black/20 z-0"></div>
 
       {/* إضاءات خلفية ناعمة جداً بألوانك المبهجة */}
       <div className="absolute -top-12 -end-12 w-72 h-72 bg-myPurple/20 rounded-full blur-3xl z-0"></div>
@@ -27,31 +34,11 @@ const GuzzanSite = () => {
       <div className="relative z-10 px-6 py-12 lg:py-0 lg:h-[400px] flex flex-col lg:flex-row items-center justify-between gap-12 max-w-6xl mx-auto">
         {/* الجانب الأيمن: النصوص وزر الانتقال */}
         <div className="space-y-5 max-w-xl text-center lg:text-start py-4">
-          <div className="inline-flex items-center gap-2 bg-gradient-to-r from-myBlue/5 to-myPurple/5 border border-myBlue/10 text-myBlue px-3 py-1 rounded-full text-xs font-semibold">
-            <span className="w-4 h-4 rounded-full bg-myPurple animate-pulse"></span>
-            <span
-              className="w-4 h-4 rounded-full bg-myPurple animate-pulse"
-              style={{ animationDelay: "0.3s" }}
-            ></span>
-            <span
-              className="w-4 h-4 rounded-full bg-myPurple animate-pulse"
-              style={{ animationDelay: "0.6s" }}
-            ></span>
-            <span
-              className="w-4 h-4 rounded-full bg-myPurple animate-pulse"
-              style={{ animationDelay: "0.9s" }}
-            ></span>
-          </div>
-
-          <h2 className="text-2xl md:text-3xl lg:text-4xl font-black text-gray-900 leading-tight">
-            {/* تصفح موقعنا{" "}
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-myGreen via-myBlue to-myPurple">
-              القديم بكامل خدماته
-            </span> */}
+          <h2 className="text-2xl md:text-3xl lg:text-4xl font-black text-white leading-tight">
             {data?.title}
           </h2>
 
-          <p className="text-sm md:text-base text-gray-700 leading-relaxed">
+          <p className="text-sm md:text-base text-white leading-relaxed">
             {data?.description}
           </p>
 
@@ -60,22 +47,11 @@ const GuzzanSite = () => {
               href={data?.url}
               target="_blank"
               rel="noopener noreferrer"
-              className="inline-flex items-center justify-center px-6 py-3 text-sm font-bold rounded-xl text-white bg-myBlue hover:bg-myBlue/90 shadow-lg shadow-myBlue/15 transition-all duration-300 active:scale-95 group gap-2"
+              className="inline-flex items-center justify-center px-6 py-3 text-sm font-bold rounded-xl text-myPurple bg-white hover:brightness-90 shadow-lg shadow-myBlue/15 transition-all duration-300 active:scale-95 group gap-2"
             >
               <span>{t("GuzzanSite.archiveBtn")}</span>
-              <svg
-                className="w-4 h-4 transform group-hover:-translate-x-1 transition-transform duration-250 ltr:rotate-180 ltr:group-hover:translate-x-1"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2.5}
-                  d="M10 19l-7-7m0 0l7-7m-7 7h18"
-                />
-              </svg>
+
+              <FaArrowRightLong className="transform group-hover:-translate-x-1 transition-transform duration-250 rtl:rotate-180 ltr:group-hover:translate-x-1" />
             </a>
           </div>
         </div>
